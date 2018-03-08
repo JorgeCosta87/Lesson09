397d1c91639e441d70606ede67ae3280453cf42a
JorgeCosta87 <jorgemdcosta@gmail.com>
2018-03-08 12:08:42 +0000

    Lesson 09 - Template

---
+++ project.c
@@ -0,0 +1,23 @@
+/*
+ * project.c
+ *
+ *  Created on: 08/03/2018
+ *      Author:
+ */
+
+
+#include <stdio.h>
+
+int main(void){
+    //declaration part
+
+    //reading values
+
+    //testing if they are valid
+
+    //computing the date
+
+    //showing the date
+
+    return 0;
+}
