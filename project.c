2118ea309ba3257b90520bd610657dd3ec1afcd2
Jorge Costa <Jorgemdcosta@gmail.com>
2018-03-08 12:01:49 +0000

    Create project.c
    Template

---
+++ project.c
@@ -0,0 +1,16 @@
+
+
+
+int main(void){
+    //declaration part
+
+    //reading values
+
+    //testing if they are valid
+
+    //computing the date
+
+    //showing the date
+    
+    return 0;
+}
